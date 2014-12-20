e441d7a6070be41b8cdea6de9b0d6081d9f38ce2
Jason Walsh <Jason.Walsh@colonyamerican.com>
2014-12-20 15:19:28 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Raspberry Pi Camera for Node.JS
+
+This script can be downloaded on your Raspberry Pi and ran to snap photos
+from the Raspberry Pi Camera and upload them to AWS S3 at a set interval
+
+You will need to create a credentials.js file in the root of the folder
+that exports an object. The object should have your AWS S3 key, secret, and bucket.
+
+Once that is created you can run 'node index' from command line and the script will start
+taking pictures and uploading your photos to S3.
+
+You can also pass a few arguments when running the script. Currently the script supports
+width, height, and delay (between images). For example 'delay=10000 width=1024 height=768 node index'
---
+++ index.js
@@ -0,0 +1,57 @@
+var spawn = require('child_process').spawn;
+var RaspiCam = require('raspicam');
+var moment = require('moment');
+var credentials = require('./credentials');
+var knox = require('knox');
+var fs = require('fs');
+
+var CMD = '/opt/vc/bin/raspistill';
+var snapshotTime = process.env.delay || 10000; // in ms
+var width = process.env.width || 640; // in pixels
+var height = process.env.height || 480;
+
+console.log('Taking snapshots every ' + snapshotTime + 'ms at ' + width + 'x' + height);
+
+function removeLocalFile(filename) {
+  fs.unlink(filename, function(err) {
+    if (err) {
+      console.log('Error removing file: ', err);
+    } else {
+      console.log('Removed local file: ', filename);
+    }
+  });
+}
+
+function uploadToS3(localPath, filename) {
+  var client = knox.createClient(credentials);
+  client.putFile(
+    localPath + filename, filename,
+    function(err, result) {
+      if (result.statusCode !== 200) {
+        err = new Error('Upload Failure: ' + result.statusCode);
+      } else {
+        removeLocalFile(localPath + filename);
+      }
+    }
+  );
+}
+
+function snapPhoto() {
+  var localPath = './images/';
+  var filename = moment().format('MM-DD-YYYY_m:hh:ss') + '.jpg';
+  var args = [
+    '--output', localPath + filename,
+    '--width', width,
+    '--height', height
+  ];
+
+  var child_process = spawn(CMD, args);
+
+  child_process.on('close', function () {
+    console.log('Image written to file: ', filename);
+    uploadToS3(localPath, filename);
+    setTimeout(snapPhoto, snapshotTime);
+  });
+}
+
+snapPhoto();
